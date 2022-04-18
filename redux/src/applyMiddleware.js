d095818e16c41b491cfd9d221b6a2c4cdbcf4be5
shaoyangyang <yangyang.shao@brgroup.com>
2022-04-18 14:26:54 +0800

    update comment

--- redux/src/applyMiddleware.js
+++ redux/src/applyMiddleware.js
@@ -4,14 +4,18 @@ import compose from './compose'
  * Creates a store enhancer that applies middleware to the dispatch method
  * of the Redux store. This is handy for a variety of tasks, such as expressing
  * asynchronous actions in a concise manner, or logging every action payload.
+ * * 创建一个store增强器，该增强器将中间件应用于Redux store的调dispatch方法。
+ * * 这对于各种任务都很方便，比如以一种简洁的方式表示异步操作，或者记录每个action payload。
  *
  * See `redux-thunk` package as an example of the Redux middleware.
  *
  * Because middleware is potentially asynchronous, this should be the first
  * store enhancer in the composition chain.
+ * * 因为中间件可能是异步的，所以这应该是组合链中的第一个存储增强器。
  *
  * Note that each middleware will be given the `dispatch` and `getState` functions
  * as named arguments.
+ * * 请注意，每个中间件将被赋予' dispatch '和' getState '函数作为命名参数。
  *
  * @param {...Function} middlewares The middleware chain to be applied.
  * @returns {Function} A store enhancer applying the middleware.
--- redux/src/combineReducers.js
+++ redux/src/combineReducers.js
@@ -14,14 +14,14 @@ function getUnexpectedStateShapeWarningMessage(
     action && action.type === ActionTypes.INIT
       ? 'preloadedState argument passed to createStore'
       : 'previous state received by the reducer'
-
+  // 没有有效的reducer
   if (reducerKeys.length === 0) {
     return (
       'Store does not have a valid reducer. Make sure the argument passed ' +
       'to combineReducers is an object whose values are reducers.'
     )
   }
-
+  // 初始state必须是一个对象
   if (!isPlainObject(inputState)) {
     return (
       `The ${argumentName} has unexpected type of "${kindOf(
@@ -30,7 +30,7 @@ function getUnexpectedStateShapeWarningMessage(
       `keys: "${reducerKeys.join('", "')}"`
     )
   }
-
+  // 找到初始state的key在reucers中无对应的key
   const unexpectedKeys = Object.keys(inputState).filter(
     (key) => !reducers.hasOwnProperty(key) && !unexpectedKeyCache[key]
   )
@@ -51,11 +51,15 @@ function getUnexpectedStateShapeWarningMessage(
   }
 }
 
+// 如果reducer的返回值是否为undefined，抛出异常
+// ? 为何返回值不可以为undefined, 可以是为了防止没有显示的返回初始状态，
 function assertReducerShape(reducers) {
   Object.keys(reducers).forEach((key) => {
     const reducer = reducers[key]
     const initialState = reducer(undefined, { type: ActionTypes.INIT })
 
+  // 如果传递给reducer的状态是未定义的，则必须显式地返回初始状态。
+  // 初始状态可能不是undefined。如果你不想为这个reducer设置一个值，你可以使用null代替undefined。
     if (typeof initialState === 'undefined') {
       throw new Error(
         `The slice reducer for key "${key}" returned undefined during initialization. ` +
@@ -88,16 +92,22 @@ function assertReducerShape(reducers) {
  * reducer function. It will call every child reducer, and gather their results
  * into a single state object, whose keys correspond to the keys of the passed
  * reducer functions.
- *
+ * * 将值为不同reducer函数的对象转换为单个reducer函数。
+ * * 它将调用每一个子reducer，并将它们的结果收集到一个单一的state中，
+ * * 该对象的键对应于传递的reducer函数的键。
  * @param {Object} reducers An object whose values correspond to different
  * reducer functions that need to be combined into one. One handy way to obtain
  * it is to use ES6 `import * as reducers` syntax. The reducers may never return
  * undefined for any action. Instead, they should return their initial state
  * if the state passed to them was undefined, and the current state for any
  * unrecognized action.
- *
+ * * 一个对象，其值对应于需要合并为一个的, 不同的 reducer 函数。 
+ * * 获得它的一种方便方法是使用 ES6 `import * as reducers` 语法。 
+ * * 对于任何操作，reducers 可能永远不会返回 undefined。 
+ * * 相反，如果传递给它们的状态是未定义的，它们应该返回它们的初始状态，以及任何无法识别的动作的当前状态。
  * @returns {Function} A reducer function that invokes every reducer inside the
  * passed object, and builds a state object with the same shape.
+ * * 一个reducer函数，它调用传递的对象内部的每个reducer，并构建具有相同形状的state对象。
  */
 export default function combineReducers(reducers) {
   const reducerKeys = Object.keys(reducers)
@@ -105,12 +115,13 @@ export default function combineReducers(reducers) {
   for (let i = 0; i < reducerKeys.length; i++) {
     const key = reducerKeys[i]
 
+    // 开发环境，对于未定义的reducer发出警告
     if (process.env.NODE_ENV !== 'production') {
       if (typeof reducers[key] === 'undefined') {
         warning(`No reducer provided for key "${key}"`)
       }
     }
-
+    // 收集类型为函数的正确的reducer
     if (typeof reducers[key] === 'function') {
       finalReducers[key] = reducers[key]
     }
@@ -119,6 +130,7 @@ export default function combineReducers(reducers) {
 
   // This is used to make sure we don't warn about the same
   // keys multiple times.
+  // * 这用于确保我们不会多次警告相同的键。
   let unexpectedKeyCache
   if (process.env.NODE_ENV !== 'production') {
     unexpectedKeyCache = {}
@@ -150,11 +162,13 @@ export default function combineReducers(reducers) {
 
     let hasChanged = false
     const nextState = {}
+    // 执行所有的reducer
     for (let i = 0; i < finalReducerKeys.length; i++) {
       const key = finalReducerKeys[i]
       const reducer = finalReducers[key]
       const previousStateForKey = state[key]
       const nextStateForKey = reducer(previousStateForKey, action)
+      // reducer不可以返回undefined
       if (typeof nextStateForKey === 'undefined') {
         const actionType = action && action.type
         throw new Error(
@@ -170,6 +184,7 @@ export default function combineReducers(reducers) {
     }
     hasChanged =
       hasChanged || finalReducerKeys.length !== Object.keys(state).length
+    // 如果state没有变化，返回原来的state，用于优化
     return hasChanged ? nextState : state
   }
 }
